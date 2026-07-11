8fcde3fadbf59b2f8d6af798b0b175a5a6c2c074
Topraus <toprak026324@gmail.com>
2026-07-11 11:05:55 +0300

    madlib game

---
+++ learning/03-madlibs_game.py
@@ -0,0 +1,9 @@
+noun1 = input("Enter a noun(singular): ")
+noun2 = input("Enter another noun(singular): ")
+adjective1 = input("Enter an adjective: ")
+verb1 = input("Enter a verb (ending with -ing): ")
+
+print(f"I dont like {noun1}.")
+print(f"Because they are {adjective1}.")
+print(f"And {noun1} was {verb1} {noun2}.")
+
