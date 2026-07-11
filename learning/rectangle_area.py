635d6891c7175ac8f29007ad11a6542f56943f27
Topraus <toprak026324@gmail.com>
2026-07-11 10:35:37 +0300

    rectangle area calculator

---
+++ learning/rectangle_area.py
@@ -0,0 +1,8 @@
+# Calculating the are of a rectangle #
+
+width = float(input("Please enter the width of the rectangle: "))
+length = float(input("Please enter the length of the rectangle: "))
+
+area = width * length 
+
+print(f"The are of the rectangle is {area}.")
